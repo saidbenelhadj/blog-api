d49d7a1468811ed46653d193826f2854cc44b1d5
said benelhadj <sad.beneldje@gmail.com>
2023-07-27 10:47:20 +0200

    install blog-api

--- src/main/java/com/formation/blogapi/entities/UserEntity.java
+++ src/main/java/com/formation/blogapi/entities/UserEntity.java
@@ -1,5 +1,11 @@
 package com.formation.blogapi.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,10 +13,19 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "user")
 public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @Column(length = 50, nullable = false, unique = true)
     private String username;
+
+    @Column(nullable = false)
     private String password;
+    
     private String passwordConfirm;
     private String email;
 

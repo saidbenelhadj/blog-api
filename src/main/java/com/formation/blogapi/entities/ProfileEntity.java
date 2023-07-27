321e95ee3f8b96a6520c4eb2306b72587826737d
said benelhadj <sad.beneldje@gmail.com>
2023-07-27 11:15:05 +0200

    create ProfileEntity

---
+++ src/main/java/com/formation/blogapi/entities/ProfileEntity.java
@@ -0,0 +1,30 @@
+package com.formation.blogapi.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "profile")
+public class ProfileEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String Bio;
+
+    @OneToOne
+    private ProfileEntity profile;
+   
+}
